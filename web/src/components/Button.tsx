26196d8ab5efb3974c1f79bcba5ea07c6ff38004
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-21 15:30:58 -0300

    feat: add type and function to buttons

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,12 +1,17 @@
 import { ReactNode } from "react"
 import { FcGoogle }  from "react-icons/fc"
+import { createAccountWithGoogle } from "../firebase/loginWithGoogleProvider"
 export interface ButtonRootProps {
   children: ReactNode
+  handleClick?(): void
 }
 
-export function ButtonAdd({children}: ButtonRootProps) {
+export function ButtonAdd({children, handleClick}: ButtonRootProps) {
   return (
-    <button className="flex hover:bg-blue-600/90 transition-all gap-3 w-full justify-center text-sm rounded items-center p-2 text-white bg-blue-600">
+    <button
+      type="button"
+      onClick={handleClick}
+      className="flex hover:bg-blue-600/90 transition-all gap-3 w-full justify-center text-sm rounded items-center p-2 text-white bg-blue-600">
       {children}
     </button>
   )
@@ -14,7 +19,10 @@ export function ButtonAdd({children}: ButtonRootProps) {
 
 export function ButtonSecondary() {
   return (
-    <button className="flex hover:bg-gray-100 border-2 border-gray-100 transition-all gap-3 w-full justify-center text-sm rounded items-center p-2 text-gray-900 bg-white">
+    <button 
+      type="button"
+      onClick={createAccountWithGoogle}
+      className="flex hover:bg-gray-100 border-2 border-gray-100 transition-all gap-3 w-full justify-center text-sm rounded items-center p-2 text-gray-900 bg-white">
       <FcGoogle className="text-2xl" />
       <span className="text-base">Entrar com o Google</span>
     </button>

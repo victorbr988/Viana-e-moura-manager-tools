53708e4a782a7140f42b046323694017e803f6bf
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-21 15:32:17 -0300

    feat: add enum with error types

---
+++ web/src/errors/customErrors.ts
@@ -0,0 +1,6 @@
+export enum customErrors {
+  NOT_FOUND = "auth/user-not-found",
+  WRONG_PASSWORD = "auth/wrong-password",
+  INVALID_EMAIL = "auth/invalid-email",
+  INVALID_PASSWORD = "auth/invalid-password"
+};

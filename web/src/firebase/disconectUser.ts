b24a93696dc44610b551962777a955c2bba65cf9
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-21 15:32:52 -0300

    feat: add function to logout user

---
+++ web/src/firebase/disconectUser.ts
@@ -0,0 +1,13 @@
+import { signOut } from "firebase/auth";
+import toast from "react-hot-toast" 
+import { auth } from "./firebase-init";
+
+export async function disconect() {
+  try {
+    signOut(auth)
+    toast.success('Usuário desconectado')
+  } catch(error) {
+    toast.error("Não foi possível desconectar-se")
+    console.log(error)
+  }
+}

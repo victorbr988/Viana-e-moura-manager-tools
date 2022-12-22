5974dfce5785a2c9fb805956a310e86a8767e723
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-22 19:08:52 -0300

    feat: add method to create account with context

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,12 +1,13 @@
-import { ReactNode } from "react"
+import { ReactNode, useContext } from "react"
 import { FcGoogle }  from "react-icons/fc"
-import { createAccountWithGoogle } from "../firebase/loginWithGoogleProvider"
+import { AuthContext } from "../context/Context-provider"
 export interface ButtonRootProps {
   children: ReactNode
   handleClick?(): void
 }
 
 export function ButtonAdd({children, handleClick}: ButtonRootProps) {
+  
   return (
     <button
       type="button"
@@ -18,10 +19,11 @@ export function ButtonAdd({children, handleClick}: ButtonRootProps) {
 }
 
 export function ButtonSecondary() {
+  const contextState = useContext(AuthContext)
   return (
     <button 
       type="button"
-      onClick={createAccountWithGoogle}
+      onClick={contextState.createAccountWithGoogle}
       className="flex hover:bg-gray-100 border-2 border-gray-100 transition-all gap-3 w-full justify-center text-sm rounded items-center p-2 text-gray-900 bg-white">
       <FcGoogle className="text-2xl" />
       <span className="text-base">Entrar com o Google</span>

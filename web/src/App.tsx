afd5db7ddf0ae13c3e148de2dd33a68b1d4213dc
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-23 15:34:13 -0300

    feat: add component default to page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,31 @@
+import { User } from "firebase/auth"
+import { Fragment, useContext } from "react"
+import { useLocation } from "react-router-dom"
+import { Unautorized } from "./components/DefaultComponent"
+import { Sidebar } from "./components/sidebar"
 import { AuthProvider } from "./context/Auth-provider"
+import { AuthContext } from "./context/Context-provider"
 import { Pagination } from "./Routes/Pagination"
 
-
 function App() {
+  const contextState = useContext(AuthContext)
+  const userCredential: User | null = contextState.userLogged
+  const router = useLocation()
+
+  function RenderIfIsLogged() {
+    if (router.pathname !== "/" && !userCredential) {
+      return <Unautorized /> 
+    }
+    return (
+      <Fragment>
+        {router.pathname !== "/" && userCredential !== null && (<Sidebar />)}
+        <Pagination />
+      </Fragment>
+    )
+  }
   return (
     <AuthProvider>
-      <Pagination />
+      {RenderIfIsLogged()}
     </AuthProvider>
   )
 }

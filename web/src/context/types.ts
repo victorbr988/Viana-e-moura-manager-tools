cf5059cd3bb973dcd8ed10ae3e368ca7bbe0d532
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-23 17:51:55 -0300

    refactor: remove state in type context

--- web/src/context/types.ts
+++ web/src/context/types.ts
@@ -7,12 +7,8 @@ export interface AuthProviderProps {
 export interface ContextProps {
   userLogged: User | null;
   setUserLogged: React.Dispatch<React.SetStateAction<User | null>>;
-  email: string;
-  setEmail: React.Dispatch<React.SetStateAction<string>>;
-  password: string;
-  setPassword: React.Dispatch<string>;
-  createUser: () => Promise<User | undefined>;
-  loading: boolean,
+  createUser: (email: string, password: string) => Promise<User | undefined>;
+  loading: boolean;
   user: UserCredential | undefined;
-  createAccountWithGoogle(): Promise<void>
+  createAccountWithGoogle(): Promise<void>;
 }

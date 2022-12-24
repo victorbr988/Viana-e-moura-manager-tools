1a1f507378193dab4ef4f7d32c9bc86b3a449381
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-24 18:58:51 -0300

    feat: render page with route exact

--- web/src/Routes/Pagination.tsx
+++ web/src/Routes/Pagination.tsx
@@ -1,5 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { HomeApp } from "../pages/Home-app";
+import { HomeAppEnterprises } from "../pages/Home-app-Enterprises";
+import { HomeAppEntrances } from "../pages/Home-app-Entrances";
+import { HomeAppExit } from "../pages/Home-app-Exit";
+import { HomeAppSupervisors } from "../pages/Home-app-Supervisors";
+import { HomeAppTools } from "../pages/Home-app-tools";
 import { HomeLogin } from "../pages/Home-login";
 
 export function Pagination() {
@@ -8,6 +13,11 @@ export function Pagination() {
     <Routes>
       <Route path="/" element={ <HomeLogin />} />
       <Route path="/home-app" element={ <HomeApp />} />
+      <Route path="/home-app/tools" element={ <HomeAppTools /> } />
+      <Route path="/home-app/enterprises" element={ <HomeAppEnterprises /> } />
+      <Route path="/home-app/supervisors" element={ <HomeAppSupervisors /> } />
+      <Route path="/home-app/entrance" element={ <HomeAppEntrances /> } />
+      <Route path="/home-app/exit" element={ <HomeAppExit /> } />
     </Routes>
   )
 }

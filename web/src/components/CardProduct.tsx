bb055fe2968fc46ee3cf4651f23b775574ebd282
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-24 18:53:37 -0300

    refactor: remove ununsed prop and replace style

--- web/src/components/CardProduct.tsx
+++ web/src/components/CardProduct.tsx
@@ -9,13 +9,12 @@ export interface DataProps {
 export interface CardProductProps {
   children: ReactNode;
   data: DataProps;
-  colorBorder?: string;
 }
 
 export function CardProduct({ children, data }: CardProductProps ) {
   return (
     <div 
-      className="flex justify-between items-center p-2 w-full border-teal-500 bg-white shadow-md border-l-8 rounded">
+      className="flex text-lg justify-between items-center p-2 w-full border-teal-500 bg-white shadow-md border-l-8 rounded">
         <h2>{data.name}</h2>
       <div className="flex justify-end items-center gap-3">
         { children }

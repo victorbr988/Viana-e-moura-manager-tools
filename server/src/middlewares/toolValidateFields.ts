072a82459a22613f0ae2aa5f2293123ca07b636d
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-25 17:57:47 -0300

    refactor: add new validations to tools

--- server/src/middlewares/toolValidateFields.ts
+++ server/src/middlewares/toolValidateFields.ts
@@ -1,15 +1,25 @@
+import { PrismaClient } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { StatusCode, ToolProps } from "../types";
 
-export function toolValidation(request: Request, response: Response, next: NextFunction) {
+export async function toolValidation(request: Request, response: Response, next: NextFunction) {
   const { name }: ToolProps = request.body;
-  const MIN_LENGTH_FIELD = 3;
+  const MIN_LENGTH_FIELD = 2;
+
+  const prisma = new PrismaClient()
+  const findToolWithName = await prisma.tools.findUnique({
+    where: { name }
+  })
+
+  if(findToolWithName) {
+    return response.status(StatusCode.INVALID_DATA).json({ message: "Nome já cadastrado"})
+  }
 
   if (!name) {
     return response.status(StatusCode.NOT_FOUND).json({ message: "É obrigatório preencher o nome"});
   };
 
-  if (name.length <= MIN_LENGTH_FIELD) {
+  if (name.length < MIN_LENGTH_FIELD) {
     return response.status(StatusCode.INVALID_DATA).json({ message: "O mínimo para nomear uma ferramenta são 3 caracteres" });
   };
 

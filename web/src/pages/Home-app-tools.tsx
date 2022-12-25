8d673b1b6d24874c08e2403fe59e3d07d6dc0e81
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-25 11:18:11 -0300

    refactor: adapt component to comunicate with API

--- web/src/pages/Home-app-tools.tsx
+++ web/src/pages/Home-app-tools.tsx
@@ -1,15 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useContext, useState } from "react";
 import { FiEdit, FiPlus, FiSearch, FiTrash2 } from "react-icons/fi";
 import { ButtonAdd } from "../components/Button";
 import { CardProduct } from "../components/CardProduct";
 import { NoData } from "../components/DefaultComponent";
 import { inputGroup } from "../components/Modal";
+import { ModalTools } from "../components/modals-manager/Tools";
+import { DatabaseContext } from "../context/Context-provider";
 
 export function HomeAppTools() {
-  const mockData: any[] = []
+  const contextState = useContext(DatabaseContext)
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
+  console.log("Chamou o homeApp")
 
-    return (
-      <section className="p-20 w-full flex flex-col gap-10">
+  return (
+    <section className="relative w-full flex flex-col">
+      <div className="p-20 flex flex-col gap-10">
         <header>
           <h1 className="text-center text-2xl font-semibold">Ferramentas de infraestrutura</h1>
         </header>
@@ -20,29 +25,44 @@ export function HomeAppTools() {
             <inputGroup.Input placeholder="Buscar por uma ferramenta" />
           </inputGroup.Label>
           <section>
-            <ButtonAdd >
+            <ButtonAdd
+              handleClick={() => setIsOpenModal(true)}
+            >
               <FiPlus />
               Adicionar
             </ButtonAdd>
           </section>
         </section>
 
-        <main className="flex flex-col gap-3 h-96 overflow-auto h-">
-          {
-            mockData.length <= 0 ? (<NoData />)
-            : mockData.map(tool => (
-              <CardProduct 
-                key={tool.id}
-                data={tool}
-              >
-                <Fragment>
-                  <FiEdit className="text-green-500 cursor-pointer" />
-                  <FiTrash2 className="text-red-500 cursor-pointer" />
-                </Fragment>
-              </CardProduct>
-            ))
-          }
+        <main>
+          <h2 className="text-gray-600 mb-1">De A-Z</h2>
+          <div className="h-96 flex flex-col gap-3 overflow-auto">
+            {
+              contextState.tools.length <= 0 ? (<NoData />)
+              : contextState.tools.map(tool => (
+                <CardProduct 
+                  key={tool.id}
+                  data={tool}
+                >
+                  <Fragment>
+                    <FiEdit 
+                      className="text-green-500 cursor-pointer"
+                    />
+                    <FiTrash2
+                      className="text-red-500 cursor-pointer"
+                      onClick={() => contextState.deleteTool(tool.id as number)}
+                    />
+                  </Fragment>
+                </CardProduct>
+              ))
+            }
+          </div>
         </main>
-      </section>
-    )
+      </div>
+      <ModalTools 
+        isOpen={isOpenModal}
+        setIsOpen={setIsOpenModal}
+      />
+    </section>
+  )
 }

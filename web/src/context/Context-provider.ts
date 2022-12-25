37056a8331c579b2caf13eadeb7fd5706e3ed885
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-25 11:16:48 -0300

    feat: create new context

--- web/src/context/Context-provider.ts
+++ web/src/context/Context-provider.ts
@@ -1,4 +1,7 @@
 import { createContext } from "react"
+import { ContextDatabaseProps } from "./Database-provider"
 import { ContextProps } from "./types"
 
 export const AuthContext = createContext({} as ContextProps)
+
+export const DatabaseContext = createContext({} as ContextDatabaseProps)

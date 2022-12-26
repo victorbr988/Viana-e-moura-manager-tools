69a9e3e112eb412cd703e044c156fd6b265a2574
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-26 11:54:46 -0300

    feat: create functions to manager enterprises

--- web/src/context/Database-provider.tsx
+++ web/src/context/Database-provider.tsx
@@ -1,9 +1,10 @@
 import { AxiosError } from "axios";
 import { ReactNode, useEffect, useState } from "react"
 import { toast } from "react-hot-toast";
+import { useLocation } from "react-router-dom";
 import { create, deleteData, getData, update } from "../axios/requesters";
 import { DatabaseContext } from "./Context-provider"
-import { ToolProps } from "./types";
+import { EnterpriseProps, ToolProps } from "./types";
 
 interface DatabaseProviderProps {
   children: ReactNode
@@ -14,17 +15,51 @@ export interface ContextDatabaseProps {
   createTool(value: string): void;
   deleteTool(id: number): void;
   updateTool(data: Record<string, string | number>): void;
+  enterprises: EnterpriseProps[]
+  createEnterprise(value: string): void;
+  deleteEnterprise(id: number): void;
+  updateEnterprise(data: Record<string, string | number>): void;
+
 }
 
 export function DatabaseProvider({ children }: DatabaseProviderProps) {
   const [tools, setTools] = useState<ToolProps[]>([])
-  console.log("Chamou o contexto")
+  const [enterprises, setEnterprises] = useState<EnterpriseProps[]>([])
 
   const contextType: ContextDatabaseProps = {
     tools,
     createTool,
     deleteTool,
-    updateTool
+    updateTool,
+    createEnterprise,
+    deleteEnterprise,
+    updateEnterprise,
+    enterprises
+  }
+
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname === '/home-app/tools') {
+      getTools()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (location.pathname === '/home-app/enterprises') {
+      getEnterprises()
+    }
+  }, [])
+
+  async function getTools() {
+    try {
+      const tools = await getData('/tools')
+      setTools(tools.data.sort())
+      return tools
+    } catch(err: AxiosError | any) {
+      toast.error(err)
+      console.log(err)
+    }
   }
 
   async function createTool(name: string) {
@@ -41,17 +76,6 @@ export function DatabaseProvider({ children }: DatabaseProviderProps) {
     } catch(err: AxiosError | any) {
       toast.dismiss(toastId)
       toast.error(err)
-    }
-  }
-
-  async function getTools() {
-    try {
-      const tools = await getData('/tools')
-      setTools(tools.data.sort())
-      return tools
-    } catch(err: AxiosError | any) {
-      toast.error(err)
-      console.log(err)
     }
   }
 
@@ -88,9 +112,66 @@ export function DatabaseProvider({ children }: DatabaseProviderProps) {
     }
   }
 
-  useEffect(() => {
-    getTools()
-  }, [])
+  async function getEnterprises() {
+    try {
+      const enterprises = await getData('/enterprises')
+      setEnterprises(enterprises.data.sort())
+      return enterprises
+    } catch(err: AxiosError | any) {
+      toast.error(err)
+      console.log(err)
+    }
+  }
+
+  async function createEnterprise(name: string) {
+    const toastId = toast.loading("Criando empreendimento...", {
+      duration: 2000
+    })
+    try {
+      await create('/enterprises', { name })
+      toast.success("Empreendimento criado com sucesso", {
+        duration: 3000
+      })
+      toast.dismiss(toastId)
+      await getEnterprises()
+    } catch(err: AxiosError | any) {
+      toast.dismiss(toastId)
+      toast.error(err)
+    }
+  }
+
+  async function deleteEnterprise(id: number) {
+    const toastId = toast.loading("Verificando empreendimento...", {
+      duration: 2000
+    })
+    try {
+      await deleteData(`/enterprises/${id}`)
+      toast.success("Excluído com sucesso")
+      await getEnterprises()
+      toast.dismiss(toastId)
+    } catch(err: AxiosError | any) {
+      toast.dismiss(toastId)
+      toast.error(err)
+      throw err
+    }
+  }
+
+  async function updateEnterprise(data: Record<string, string | number>) {
+    const toastId = toast.loading("Editando empreendimento...", {
+      duration: 2000
+    })
+    try {
+      await update(`/enterprises/${data.id}`, data)
+      await getEnterprises()
+
+      toast.dismiss(toastId)
+      toast.success("Editado com sucesso")
+    } catch(err: AxiosError | any) {
+      toast.dismiss(toastId)
+      toast.error(err)
+      throw err
+    }
+  }
 
   return (
     <DatabaseContext.Provider value={contextType}>

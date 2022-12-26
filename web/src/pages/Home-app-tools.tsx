095ed4fc2b62a6203ce19a7b5c2650a87cb90170
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-25 21:20:51 -0300

    feat: create page to manager tools

--- web/src/pages/Home-app-tools.tsx
+++ web/src/pages/Home-app-tools.tsx
@@ -4,13 +4,53 @@ import { ButtonAdd } from "../components/Button";
 import { CardProduct } from "../components/CardProduct";
 import { NoData } from "../components/DefaultComponent";
 import { inputGroup } from "../components/Modal";
-import { ModalTools } from "../components/modals-manager/Tools";
+import { ModalTools } from "../components/modals-manager/InsertTool";
+import { ModalUpdateTool } from "../components/modals-manager/UpdateTool";
 import { DatabaseContext } from "../context/Context-provider";
+import { ToolProps } from "../context/types";
+import { capitalizeFirstLetter } from "../utils/capitalizerString";
 
 export function HomeAppTools() {
   const contextState = useContext(DatabaseContext)
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
-  console.log("Chamou o homeApp")
+  const [isOpenEditModal, setIsOpenEditModal] = useState<boolean>(false)
+  const [tool, setTool] = useState<ToolProps>({} as ToolProps)
+  const [searchTool, setSearchTool] = useState<string>("")
+
+  const filteredByNameTool: ToolProps[] = contextState.tools.filter((tool) => tool.name.includes(capitalizeFirstLetter(searchTool)))
+
+  function handleClickEdit(tool: ToolProps) {
+    setIsOpenEditModal(!isOpenEditModal)
+    setTool(tool)
+  }
+
+  function handleSearch({ target }: any) {
+    setSearchTool(target.value)
+  }
+
+  function renderCards() {
+    const iterableData: ToolProps[] = filteredByNameTool.length <= 0 ? contextState.tools : filteredByNameTool
+
+    if (contextState.tools.length <= 0) return <NoData />
+    
+    return iterableData.map(tool => (
+      <CardProduct 
+        key={tool.id}
+        data={tool}
+      >
+        <Fragment>
+          <FiEdit 
+            className="text-green-500 cursor-pointer"
+            onClick={() => handleClickEdit(tool)}
+          />
+          <FiTrash2
+            className="text-red-500 cursor-pointer"
+            onClick={() => contextState.deleteTool(tool.id as number)}
+          />
+        </Fragment>
+      </CardProduct>
+    ))
+  }
 
   return (
     <section className="relative w-full flex flex-col">
@@ -22,7 +62,7 @@ export function HomeAppTools() {
         <section className="flex flex-col w-full gap-2 items-end bg-transparent">
           <inputGroup.Label>
             <FiSearch />
-            <inputGroup.Input placeholder="Buscar por uma ferramenta" />
+            <inputGroup.Input onChange={handleSearch} placeholder="Buscar por uma ferramenta" />
           </inputGroup.Label>
           <section>
             <ButtonAdd
@@ -38,23 +78,7 @@ export function HomeAppTools() {
           <h2 className="text-gray-600 mb-1">De A-Z</h2>
           <div className="h-96 flex flex-col gap-3 overflow-auto">
             {
-              contextState.tools.length <= 0 ? (<NoData />)
-              : contextState.tools.map(tool => (
-                <CardProduct 
-                  key={tool.id}
-                  data={tool}
-                >
-                  <Fragment>
-                    <FiEdit 
-                      className="text-green-500 cursor-pointer"
-                    />
-                    <FiTrash2
-                      className="text-red-500 cursor-pointer"
-                      onClick={() => contextState.deleteTool(tool.id as number)}
-                    />
-                  </Fragment>
-                </CardProduct>
-              ))
+              renderCards()
             }
           </div>
         </main>
@@ -62,6 +86,11 @@ export function HomeAppTools() {
       <ModalTools 
         isOpen={isOpenModal}
         setIsOpen={setIsOpenModal}
+      />
+      <ModalUpdateTool
+        toolId={tool.id as number}
+        isOpen={isOpenEditModal}
+        setIsOpen={setIsOpenEditModal}
       />
     </section>
   )

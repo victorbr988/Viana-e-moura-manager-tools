8873d7542438ef2048bb03ceedea019a41dfecf7
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-26 11:51:52 -0300

    improve: verify if exist enterprise equal name

--- server/src/middlewares/enterpriseValidateFields.ts
+++ server/src/middlewares/enterpriseValidateFields.ts
@@ -1,9 +1,19 @@
+import { PrismaClient } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { EnterpriseProps, StatusCode } from "../types";
 
-export function enterpriseValidation(request: Request, response: Response, next: NextFunction) {
+export async function enterpriseValidation(request: Request, response: Response, next: NextFunction) {
   const { name }: EnterpriseProps = request.body;
   const MIN_LENGTH_FIELD = 3;
+
+  const prisma = new PrismaClient()
+  const findEnterpriselWithName = await prisma.enterprises.findUnique({
+    where: { name }
+  })
+
+  if(findEnterpriselWithName) {
+    return response.status(StatusCode.INVALID_DATA).json({ message: "Nome já cadastrado"})
+  }
 
   if (!name) {
     return response.status(StatusCode.NOT_FOUND).json({ message: "É obrigatório preencher o nome"});

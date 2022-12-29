f63479e4fd1689075f7704270d346708a3fb9095
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-28 23:31:38 -0300

    style: add title  card with sector from supervisor

--- web/src/components/CardProduct.tsx
+++ web/src/components/CardProduct.tsx
@@ -14,8 +14,12 @@ export interface CardProductProps {
 export function CardProduct({ children, data }: CardProductProps ) {
   return (
     <div 
-      className="flex text-lg justify-between items-center p-2 w-full border-blue-500 bg-white shadow-md border-l-8 rounded">
+      className="flex text-lg justify-between items-end p-2 w-full border-blue-500 bg-white shadow-md border-l-8 rounded"
+    > 
+      <div className="flex flex-col gap-1">
+        {data.sector && <p className="text-gray-500 text-sm font-light rounded-lg">Setor: {data.sector}</p>}
         <h2>{data.name}</h2>
+      </div>  
       <div className="flex justify-end items-center gap-3">
         { children }
       </div>

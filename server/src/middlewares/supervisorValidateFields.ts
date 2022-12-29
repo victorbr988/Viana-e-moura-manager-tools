e2ca46d3f86437101b5e273ea0336cec98dafc86
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-28 23:28:59 -0300

    feat: add verification if supervisor exists

--- server/src/middlewares/supervisorValidateFields.ts
+++ server/src/middlewares/supervisorValidateFields.ts
@@ -1,9 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { StatusCode, SupervisorProps, ToolProps } from "../types";
 
-export function supervisorValidation(request: Request, response: Response, next: NextFunction) {
+export async function supervisorValidation(request: Request, response: Response, next: NextFunction) {
   const { name, sector }: SupervisorProps = request.body;
   const MIN_LENGTH_FIELD = 3;
+
+  const prisma = new PrismaClient()
+  const findSupervisorWithName = await prisma.supervisors.findUnique({
+    where: { name }
+  })
+
+  if(findSupervisorWithName) return response.status(StatusCode.INVALID_DATA).json({message: "Supervisor já cadastrado"})
 
   if (!name) {
     return response.status(StatusCode.NOT_FOUND).json({ message: "É obrigatório preencher o nome"});

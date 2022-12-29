dc40f2efb4e36829d019f734fb7029bd60c7fbcc
Victor Gabriel <gabrielvghs666@gmail.com>
2022-12-28 23:34:42 -0300

    feat: add all types sector to select component

---
+++ web/src/utils/selectOptions.ts
@@ -0,0 +1,6 @@
+export const SelectOptions = [
+  {value: "Setor", label: "Setor"},
+  {value: "Infraestrutura", label: "Infraestrutura"},
+  {value: "Produção", label: "Produção"},
+  {value: "Suprimentos", label: "Suprimentos"},
+]

854e31ed0f474b06f23288fb34fffde2d7aa1dc6
Victor Gabriel <gabrielvghs666@gmail.com>
2023-01-03 20:44:02 -0300

    refactor: sepear responsabilities

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,34 @@
 import { User } from "firebase/auth"
 import { Fragment, useContext } from "react"
-import { useLocation } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import { Unautorized } from "./components/DefaultComponent"
 import { Sidebar } from "./components/sidebar"
 import { AuthContext } from "./context/Context-provider"
 import { DatabaseProvider } from "./context/Database-provider"
 import { Pagination } from "./Routes/Pagination"
 
-function App() {
+function RenderIfIsLogged() {
   const contextState = useContext(AuthContext)
   const userCredential: User | null = contextState.userLogged
   const router = useLocation()
 
-  function RenderIfIsLogged() {
-    if (router.pathname !== "/" && !userCredential) {
-      return <Unautorized /> 
-    }
-    return (
-      <DatabaseProvider>
-        <Fragment>
-          {router.pathname !== "/" && userCredential !== null && (<Sidebar />)}
-          <Pagination />
-        </Fragment>
-      </DatabaseProvider>
-    )
+  if (router.pathname !== "/" && !userCredential) {
+    return <Unautorized /> 
   }
   return (
+    <DatabaseProvider>
+      <Fragment>
+        {router.pathname !== "/" && userCredential !== null && (<Sidebar />)}
+        <Pagination />
+      </Fragment>
+    </DatabaseProvider>
+  )
+}
+
+function App() {
+  return (
     <div className="flex">
-      {RenderIfIsLogged()}
+      <RenderIfIsLogged />
     </div>
   )
 }
